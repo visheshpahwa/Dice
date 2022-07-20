3defa2625d47ec60bfabf3dbe32276e42d4a24e4
Vishesh Pahwa <73529277+visheshpahwa@users.noreply.github.com>
2022-07-20 15:10:52 +0530

    resolved img error

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var randomNumber1 = Math.floor(Math.random() * 6)+1;
 let diceImage1="dice"+randomNumber1+".png";
-var randomImageSource1="/"+diceImage1;
+var randomImageSource1=diceImage1;
 var image1= document.querySelectorAll("img")[0];
 image1.setAttribute("src",randomImageSource1);
 
 
 var randomNumber2 = Math.floor(Math.random() * 6)+1;
 let diceImage2="dice"+randomNumber2+".png";
-var randomImageSource2="/"+diceImage2;
+var randomImageSource2=diceImage2;
 document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 
