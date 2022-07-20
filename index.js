9ca428f4c062cf927cbe1a2fd0a535723ad2f3de
Vishesh Pahwa <73529277+visheshpahwa@users.noreply.github.com>
2022-07-20 15:06:01 +0530

    commit 2

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
-var randomNumber1 = Math.floor((Math.random() * 6)+1);
-var randomNumber2 = Math.floor((Math.random() * 6)+1);
-
-var diceImage1="/images/dice"+randomNumber1+".png";
-var diceImage2="/images/dice"+randomNumber2+".png";
-
-
+var randomNumber1 = Math.floor(Math.random() * 6)+1;
+let diceImage1="dice"+randomNumber1+".png";
+var randomImageSource1="/"+diceImage1;
 var image1= document.querySelectorAll("img")[0];
-image1=setAttribute("src",diceImage1);
+image1.setAttribute("src",randomImageSource1);
 
-var image2= document.querySelectorAll("img")[1].setAttribute("src",diceImage2);
+
+var randomNumber2 = Math.floor(Math.random() * 6)+1;
+let diceImage2="dice"+randomNumber2+".png";
+var randomImageSource2="/"+diceImage2;
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
 
 
 
 if (randomNumber1>randomNumber2) {
     
-    document.querySelector("h1").innerHTML("Player 1 won")
+    document.querySelector("h1").innerHTML="✌️Player 1 won"
 }
 else if(randomNumber1<randomNumber2){
     
-    document.querySelector("h1").innerHTML("Player 2 won")
+    document.querySelector("h1").innerHTML="Player 2 won✌️"
 }
 else{
     
-    document.querySelector("h1").innerHTML("Draw!s")
+    document.querySelector("h1").innerHTML="Draw!"
 }
+
